efcc419fbebe707e0f10d52fe61ade32710a68a0
Shilpajordan <Shilpa.nair@dci-student.org>
2024-03-15 15:45:53 +0100

    modified the views

--- weather/views.py
+++ weather/views.py
@@ -6,42 +6,6 @@ from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 
 # # Create your views here.
 
-# """the receiver of the request"""
-# BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
-
-# def get_weather(request):
-#     if request.method == 'POST':
-#         api_key = os.getenv('API_KEY')
-#         city = request.POST.get('city')
-#         request_url = f"{BASE_URL}?appid={api_key}&q={city}"
-#         response_data = requests.get(request_url)
-#         if response_data.status_code == 200:
-#             data = response_data.json()
-#             weather = data['weather'][0]['description']
-#             temperature = round(data['main']['temp'] - 273.15, 2)
-#             return render(request, 'weather/weather.html', {'weather': weather, 'temperature': temperature})
-#         else:
-#             error_message = 'An error occurred'
-#             return render(request, 'weather/error.html', {'error_message': error_message})
-#     return render(request, 'weather/form.html')
-
-
-# def do_geocode(address, attempt=1, max_attempts=5):
-#     geolocator = Nominatim(user_agent="my_weather_app_v1")
-#     try:
-#         return geolocator.geocode(address)
-#     except (GeocoderTimedOut, GeocoderUnavailable):
-#         if attempt <= max_attempts:
-#             return do_geocode(address, attempt=attempt+1)  # retry
-#         raise
-# city_name = input('Enter city :')  # replace with your city name
-# location = do_geocode(city_name)
-# if location:
-#     print(location.address)
-#     print((location.latitude, location.longitude))
-#     # Perform additional actions with `location` here
-# else:
-#     print(f"Location for '{city_name}' not found.")
 
 # Constants
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
